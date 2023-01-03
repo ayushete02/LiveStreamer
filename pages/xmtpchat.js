0381598f22a782fd03b083f2e2edebddb6c57bfe
Ayush Shete <ayushete02@gmail.com>
2023-01-03 15:53:46 +0530

    minor changes

--- pages/xmtpchat.js
+++ pages/xmtpchat.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Navbar from '../components/navbar'
 const XMTPchat = () => {
   return (
-    <div className='shadow h-screen'>
+    <div className='shadow h-screen overflow-hidden'>
          <Navbar/>
         <div className='h-12 z-20  bg-white w-1/2 absolute' ></div>
-      <iframe src="https://xmtp-quickstart-react.vercel.app/" title="description" className='m-auto' width='70%' height='100%'></iframe>
+      <iframe src="https://xmtp-quickstart-react.vercel.app/" title="description" className=' m-auto' width='83%' height='100%'></iframe>
     </div>
   );
 }
